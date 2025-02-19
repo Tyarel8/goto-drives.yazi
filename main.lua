bc28fc86d13d8df7853be19d77a52668d7b25d4b
Tyarel8 <98483313+Tyarel8@users.noreply.github.com>
2025-02-19 18:53:12 +0100

    feat: include drive names in the menu

--- main.lua
+++ main.lua
@@ -3,10 +3,28 @@ local function directory_exists(path)
     return files ~= nil
 end
 
+local function get_drive_name(drive)
+    -- with Command():output() it errors saying the command doesn't exist
+    local handle = io.popen("vol " .. drive)
+    local output = handle:read("*a")
+    handle:close()
+
+    if not output then
+        return nil, "Failed to execute vol command"
+    end
+
+    -- Parse the output to extract the drive name
+    local output = output:sub(1, output:find("\n") - 1)
+    local drive_name = output:match("is%s*([^%s][^%n]*)")
+
+    -- Return the drive name
+    return drive_name, nil
+end
+
 return {
     entry = function(self, job)
         if ya.target_family() ~= "windows" then
-            ya.notify { title = "goto-drives", content = "This plugin only works on windows", timeout = 3.0, level = "error" }
+            ya.notify { title = "goto-drives", content = "This plugin only works on windows", timeout = 3.0, level = "Error" }
             return
         end
 
@@ -16,24 +34,32 @@ return {
             "T", "U", "V", "W", "X", "Y", "Z", }
 
         local drives = {}
-        for _, d in ipairs(abc) do
-            if directory_exists(d .. ":") then
-                table.insert(drives, d)
+        local keys = {}
+        for _, l in ipairs(abc) do
+            local drive = l .. ":"
+            if directory_exists(drive) then
+                local name, err = get_drive_name(drive)
+                local key = string.format("%s (%s)", name or "<name>", drive)
+                drives[key] = drive
+                -- Insert the keys into an array to preserve the order
+                table.insert(keys, key)
             end
         end
 
         local permit = ya.hide()
-        local child = Command("fzf"):args({ "--prompt", "Choose a drive: " }):stdout(Command.PIPED):stdin(Command.PIPED)
+        local child = Command("fzf")
+            :args({ "--prompt", "Choose a drive: " })
+            :stdout(Command.PIPED):stdin(Command.PIPED)
             :spawn()
 
-        child:write_all(table.concat(drives, "\n"))
+        child:write_all(table.concat(keys, "\n"))
         child:flush()
 
         local output, err = child:wait_with_output()
         permit:drop()
 
         if output.stdout ~= "" then
-            ya.manager_emit("cd", { output.stdout:gsub("%s+$", "") .. ":" })
+            ya.manager_emit("cd", { drives[output.stdout:gsub("%s+$", "")] })
         end
     end,
 }
